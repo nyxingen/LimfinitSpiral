6bc099c9a41a7580130b193efc7c8372498ddc79
Uriel <wurifrorek@gmail.com>
2016-08-06 21:57:32 +0200

    Uso del Teclado, correción de grosor y colores

--- PhiOrigins.js
+++ PhiOrigins.js
@@ -1,6 +1,6 @@
 
 
-// Variables
+// Variables Globales
 
 
 var NumRadio = 0.2; // def = 0.75 ó 0.2
@@ -8,6 +8,15 @@ var contraste = 0; // 1 = Fondo blanco 0 = Fondo negro
 var PrecisionSpeed = 1000; // More = Slow
 var experimental = 1; // 1 = Uso de metodos matematicos 0 = original
 var magic = Random(0,100000000000);
+var stop = true;
+var grosor = 0.5;
+var red = 0;
+var green = 0;
+var blue = 0;
+var ChangeColor_ratio = 0;
+var ChangeColor_i = 0;
+var Size, SizeSpiral,memopathX, memopathY;
+var miscolores = 'rgba(255,255,255, 1)';
 
 
 
@@ -63,6 +72,7 @@ function AltoY(a,b){
 
 function Actualizar(){
   magic = Random(0,100000000000);
+  draw();
 }
 
 function ManualSeed(){
@@ -74,22 +84,72 @@ function ActualizarNumeros(){
     a.innerHTML =  '<input type="number" id="seed" value="' + magic + '">';
 }
 
-function start()
+
+function teclado(event) {
+  var x = event.which;
+  if (x == 97){ Height = Height + 50; } // Tecla A
+  if (x == 115){ Height = Height - 50; } // Tecla S
+  draw();
+}
+
+function Stop(){
+  if (stop == false) {
+    stop = true;
+  } else {
+    stop = false;
+  }
+}
+
+
+function animation(){
+  if (stop == false) {
+    draw();
+  }
+}
+
+
+function RandomColor()
+{
+    red = Math.floor(Math.random() * 255);
+    green = Math.floor(Math.random() * 255);
+    blue = Math.floor(Math.random() * 255);
+    //var decColor =0x1000000+ blue + 0x100 * green + 0x10000 *red ;
+    //return '#'+decColor.toString(16).substr(1);
+    return 'rgba(' +red+','+green+','+blue+', 1)';
+}
+
+function ChangeColor()
+{
+    if (ChangeColor_i == SizeSpiral){ ChangeColor_i = 0; }
+    ChangeColor_ratio = ChangeColor_i/SizeSpiral;
+    red = 256*ChangeColor_ratio;
+    green = 256*ChangeColor_ratio;
+    blue = 256*ChangeColor_ratio;
+    ChangeColor_i++;
+    //var decColor =0x1000000+ blue + 0x100 * green + 0x10000 *red ;
+    //return '#'+decColor.toString(16).substr(1);
+    console.log('rgba(' +red.toFixed()+','+green.toFixed()+','+blue.toFixed()+', 1)');
+    return 'rgba(' +red.toFixed()+','+green.toFixed()+','+blue.toFixed()+', 1)';
+}
+
+function draw()
 {
   ActualizarNumeros();
-  var Size = document.getElementById("size").value; // def = 0.6 ó 2
-  var SizeSpiral = Size * Height;
+  Size = document.getElementById("size").value; // def = 0.6 ó 2
+  if (stop){var grosor = (document.getElementById("grosor").value / 10);}
+  else {var grosor = (document.getElementById("grosor").value / 10);}
+  SizeSpiral = Size * Height;
   magic = parseFloat(document.getElementById("seed").value) + (Phi / PrecisionSpeed); // Animation
   var canvas = document.getElementById('spiral');
   var context = canvas.getContext("2d");
   var radio = 0.75;
   var angulo = (Phi * magic) / 50;
   context.clearRect(0, 0, canvas.width, canvas.height);
-  context.lineWidth = 1;
+  context.lineWidth = grosor;
   context.lineJoin="round";
-  context.strokeStyle = "#FFFFFF";
-  context.beginPath();
-  context.moveTo(canvas.width / 2, canvas.height / 2);
+  memopathX = canvas.width / 2;
+  memopathY = canvas.height / 2;
+
 
   for (var n = 0; n < SizeSpiral; n++) {
     radio += NumRadio;
@@ -99,18 +159,38 @@ function start()
     }
     var x = canvas.width / 2 + LargoX(radio,angulo);
     var y = canvas.height / 2 + AltoY(radio,angulo);
+    context.beginPath();
+    context.moveTo(memopathX, memopathY);
     context.lineTo(x, y);
+    memopathX = x;
+    memopathY = y;
+
+
+    if (n==200){miscolores = RandomColor();}
+    if (n==400){miscolores = RandomColor();}
+    if (n==600){miscolores = RandomColor();}
+    if (n==800){miscolores = RandomColor();}
+    if (n==1000){miscolores = RandomColor();}
+
+//    if (stop) {
+  //    var miscolores = RandomColor();
+      context.strokeStyle = miscolores;
+      context.fillStyle = miscolores;
+      context.stroke();
+  //  }
   }
 
   if (contraste == 1){
     context.fillStyle = '#000'; // verde
     context.fill();
   }
-  context.stroke();
+
+if (stop == false) {context.stroke();}
 
 
 
 }
 
+draw();
 
-setInterval("start()",IntervalSpeed);
+ setInterval("animation()",IntervalSpeed);
